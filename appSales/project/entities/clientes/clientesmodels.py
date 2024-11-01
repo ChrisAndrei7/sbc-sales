1c6a106b114f7ba6ec1799ae4a87c26227797a06
chris <christian_inde@hotmail.com>
2024-10-31 21:07:25 -0300

    ajuste readme

---
+++ appSales/project/entities/clientes/clientesmodels.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Cliente(models.Model):
+    NomeCliente = models.CharField(max_length=250)
+    cpf = models.CharField(max_length=15)
+    placa = models.CharField(max_length=250)
+    veiculo = models.CharField(max_length=250)
+    dataVenda = models.CharField(max_length=250)
